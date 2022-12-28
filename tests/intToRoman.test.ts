3db8345b162624e53cd32dc47e7f729f4eb5d4e4
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2022-12-28 19:46:12 +0100

    feature: convert the basic numbers In this commit  i solved the problem of converting an integer into a simple roman numbers, the integer numbers that can be converted now are the following: 1, 4, 5, 9, 10, 40, 50, 90, 100

--- tests/intToRoman.test.ts
+++ tests/intToRoman.test.ts
@@ -1,5 +1,91 @@
+import { HttpRequest } from "../common/types";
+import intToRoman from "../useCases/convertion";
+
 describe("Should run", () => {
   test("This test should be accepted", () => {
     expect(1+1).toEqual(2)
   });
 });
+
+describe("Basic test cases", () => {
+  test("Should return I", () => {
+    const httpRequest =  {
+      query:  {
+        number: "1"
+      }
+    }
+    expect(intToRoman(httpRequest)).toEqual("I")
+  })
+
+  test("Should return IV", () => {
+    const httpRequest =  {
+      query:  {
+        number: "4"
+      }
+    }
+    expect(intToRoman(httpRequest)).toEqual("IV")
+  })
+
+  test("Should return V", () => {
+    const httpRequest =  {
+      query:  {
+        number: "5"
+      }
+    }
+    expect(intToRoman(httpRequest)).toEqual("V")
+  })
+
+  test("Should return IX", () => {
+    const httpRequest =  {
+      query:  {
+        number: "9"
+      }
+    }
+    expect(intToRoman(httpRequest)).toEqual("IX")
+  })
+
+  test("Should return X", () => {
+    const httpRequest =  {
+      query:  {
+        number: "10"
+      }
+    }
+    expect(intToRoman(httpRequest)).toEqual("X")
+  })
+
+  test("Should return XL", () => {
+    const httpRequest =  {
+      query:  {
+        number: "40"
+      }
+    }
+    expect(intToRoman(httpRequest)).toEqual("XL")
+  })
+
+  test("Should return L", () => {
+    const httpRequest =  {
+      query:  {
+        number: "50"
+      }
+    }
+    expect(intToRoman(httpRequest)).toEqual("L")
+  })
+
+  test("Should return XC", () => {
+    const httpRequest =  {
+      query:  {
+        number: "90"
+      }
+    }
+    expect(intToRoman(httpRequest)).toEqual("XC")
+  })
+
+  test("Should return C", () => {
+    const httpRequest =  {
+      query:  {
+        number: "100"
+      }
+    }
+    expect(intToRoman(httpRequest)).toEqual("C")
+  })
+})
--- useCases/convertion/index.ts
+++ useCases/convertion/index.ts
@@ -1,9 +1,21 @@
 import { HttpRequest } from "../../common/types";
 
 const intToRoman = ({ query }: HttpRequest) => {
-  let number = query.number
-  let roman = ""
-  return roman;
+  let number = parseInt(query.number)
+  switch(number) {
+    case 0: return ""
+    case 1: return "I"
+    case 4: return "IV"
+    case 5: return "V"
+    case 9: return "IX"
+    case 10: return "X"
+    case 40: return "XL"
+    case 50: return "L"
+    case 90: return "XC"
+    case 100: return "C"
+
+    default: return ""
+  }
 };
 
 export default intToRoman
